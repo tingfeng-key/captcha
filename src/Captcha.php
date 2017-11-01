ecd9b0294a3a5968602b0d86bce225bb7269c445
tingfeng-key <1770963469@qq.com>
2017-11-02 01:04:09 +0800

    修改ajax无法验证的问题

--- src/Captcha.php
+++ src/Captcha.php
@@ -419,10 +419,11 @@ class Captcha
         {
             $value = $this->str->lower($value);
         }
-
-        $this->session->remove('captcha');
-
-        return $this->hasher->check($value, $key);
+        $res = $this->hasher->check($value, $key);
+        if($res){
+            $this->session->remove('captcha');
+        }
+        return $res;
     }
 
     /**
